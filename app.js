8fcef9ef004ceefe4cd565594b9e7fe39c966bd4
Naiim Taefy <naiim.tj@gmail.com>
2023-08-19 01:45:30 +0200

    feat: All works, NOT DESING

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+/** Apply configs **/
+require("./config/db.config");
+const session = require("./config/session.config");
+require("./config/hbs.config");
+
+/** Configure view engine **/
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+/** logger */
+app.use(morgan("dev"));
+
+/** session middleware */
+app.use(session);
+
+/** Support req.body **/
+app.use(express.urlencoded({ extended: true }));
+
+/** Congiure static files */
+app.use(express.static("public"));
+
+/** Routes */
+const router = require("./config/routes.config");
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Ready!");
+});
